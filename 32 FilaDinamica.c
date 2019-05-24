a61268d78c7d094ae9e99566763f95c0d60fbba0
João D. Batista <joao42lbatista@gmail.com>
2019-05-24 10:53:49 -0300

    32 FilaDinamica.c - indent --style=java

--- 32 FilaDinamica.c
+++ 32 FilaDinamica.c
@@ -1,156 +1,169 @@
 #include<stdio.h>
 #include<string.h>
 
-#include "0 os.h" //To use clean_output() and clean_stdin()
+#include "0 os.h" // To use clean_output() and clean_stdin()
 
-typedef struct sAluno{
+typedef struct sAluno {
     char nome[100];
     int idade;
-}Aluno;
+} Aluno;
 
-typedef struct sCelula{
+typedef struct sCelula {
     Aluno info;
     struct sCelula *next;
-}Celula;
+} Celula;
 
-typedef struct sFila{
+typedef struct sFila {
     Celula* ini;
     Celula* fim;
-}Fila;
+} Fila;
 
-void inicializar(Fila* fila){
+void inicializar(Fila* fila) {
     fila->ini = NULL;
     fila->fim = NULL;
 }
 
-int filaVazia(Fila* fila){
-    if(fila->ini == NULL && fila->fim == NULL){
+int filaVazia(Fila* fila) {
+    if(fila->ini == NULL && fila->fim == NULL)
         return 1;
-    }
+
     return 0;
 }
 
-Celula* criarCelula(){
-    return (Celula*) malloc (sizeof(Celula));
+Celula* criarCelula() {
+    return (Celula*) malloc(sizeof(Celula));
 }
 
-Aluno criarElemento(char nome[], int idade){
+Aluno criarElemento(char nome[], int idade) {
     Aluno e;
     strcpy(e.nome, nome);
     e.idade = idade;
     return e;
 }
 
-int inserirNaFila(Fila* fila, Aluno inserido){
+int inserirNaFila(Fila* fila, Aluno inserido) {
     Celula *nova = criarCelula();
-    if(nova == NULL){
+
+    if(nova == NULL)
         return 0;
-    }
+
     nova->info = criarElemento(inserido.nome, inserido.idade);
     nova->next = NULL;
-    if(fila->ini == NULL && fila->fim == NULL){
+
+    if(fila->ini == NULL && fila->fim == NULL) {
         fila->ini = nova;
         fila->fim = nova;
         return 1;
     }
+
     fila->fim->next = nova;
     fila->fim = nova;
     return 1;
 }
 
-Aluno removerNaFila(Fila* fila){
+Aluno removerNaFila(Fila* fila) {
     Aluno removido = criarElemento("",-1);
-    if(fila->ini == NULL && fila->fim == NULL){
+
+    if(fila->ini == NULL && fila->fim == NULL)
         return removido;
-    }
-    if(fila->ini == fila->fim){
-        removido =  criarElemento(fila->fim->info.nome,fila->fim->info.idade);
+
+    if(fila->ini == fila->fim) {
+        removido =  criarElemento(fila->fim->info.nome, fila->fim->info.idade);
         fila->ini = NULL;
         fila->fim = NULL;
         return removido;
     }
+
     Celula* removida = fila->ini;
-    removido = criarElemento(fila->ini->info.nome,fila->ini->info.idade);
+    removido = criarElemento(fila->ini->info.nome, fila->ini->info.idade);
     fila->ini = fila->ini->next;
     return removido;
 }
 
-void Menu(){
+void Menu() {
     printf("\n::INSERIR\n");
     printf("  [1] - Enfileirar\n");
-
     printf("\n::REMOVER\n");
     printf("  [2] - Desenfileirar\n");
-
     printf("\n::MOSTRAR\n");
     printf("  [3] - Imprimir\n\n");
-
     printf("::SAIR\n");
     printf("  [0] - Sair\n\n");
 }
 
-void imprimirTodos(Fila* fila){
+void imprimirTodos(Fila* fila) {
     printf("Imprimir fila:\n");
-    if(filaVazia(fila)){
+
+    if(filaVazia(fila)) {
         printf("\nFila vazia\n");
         return;
     }
+
     Fila aux;
     inicializar(&aux);
-    while(!filaVazia(fila)){
-        printf("Nome: %s - %d\n",(fila->ini)->info.nome,(fila->ini)->info.idade);
+
+    while(!filaVazia(fila)) {
+        printf("Nome: %s - %d\n",(fila->ini)->info.nome, (fila->ini)->info.idade);
         inserirNaFila(&aux, removerNaFila(fila));
     }
-    while(!filaVazia(&aux)){
+
+    while(!filaVazia(&aux))
         inserirNaFila(fila, removerNaFila(&aux));
-    }
 }
 
-int main(){
+int main() {
     Aluno e;
     Fila fila;
     inicializar(&fila);
     int option;
-    do{
+
+    do {
         Menu();
         printf("Digite uma opcao: ");
         scanf("%d", &option);
-
         clean_output();
         clean_stdin();
 
-        switch(option){
+        switch(option) {
             case 0:
                 return 0;
+
             case 1:
                 printf("-------  ENFILEIRANDO  ------\n\n");
                 printf("Digite o nome: ");
                 scanf("%s", e.nome);
                 printf("Digite o idade: ");
                 scanf("%d", &e.idade);
-                if(inserirNaFila(&fila, e)){
+
+                if(inserirNaFila(&fila, e))
                     printf("\nAluno inserido :)\n");
-                }else{
+                else
                     printf("\nOcorreu um erro :(\n");
-                }
+
                 break;
+
             case 2:
                 printf("-------  DESENFILEIRANDO  ------\n\n");
                 e = removerNaFila(&fila);
-                if(e.idade != -1){
+
+                if(e.idade != -1)
                     printf("\nAluno retirado: %s\n",e.nome);
-                }else{
+                else
                     printf("\nfila Vazia :(\n");
-                }
+
                 imprimirTodos(&fila);
                 break;
+
             case 3:
                 imprimirTodos(&fila);
                 break;
+
             default:
                 printf("\nOpcao invalida!\n");
         }
+
         printf("\n");
-    }while(1);
+    } while(1);
+
     return 0;
 }
