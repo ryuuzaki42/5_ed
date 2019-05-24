91c7c529b6e4965d26fefe9719a8c7481cfb45c6
João D. Batista <joao42lbatista@gmail.com>
2019-05-24 10:18:31 -0300

    0 os.h - indent astyle --style=java --break-blocks --delete-empty-lines --unpad-paren --remove-braces --convert-tabs --indent-switches 0 os.h

--- 0 os.h
+++ 0 os.h
@@ -1,26 +1,27 @@
 #include<stdlib.h>
 
-// --- Verifica o Sistema Operacional
+// Verifica o Sistema Operacional
 #if defined(_WIN32) || defined(_WIN64)
-    const int os=1; //Windows
+    const int os = 1; // Windows
 #else
     #ifdef __linux
-        const int os=2; // Linux
+        const int os = 2; // Linux
     #else
-        const int os=0; //Unknown
+        const int os = 0; //U nknown
     #endif
 #endif
 
-//--- Limpa buffer do teclado para Windows e GNU/Linux
-void clean_stdin(){
+// Limpa buffer do teclado para Windows e GNU/Linux
+void clean_stdin() {
     int c;
+
     while((c = getchar()) != '\n' && c != EOF);
 }
 
-void clean_output(){
-    if(os == 1){
+// Limpa o terminal para Windows e GNU/Linux
+void clean_output() {
+    if(os == 1)
         system("cls");
-    }else if(os == 2){
+    else if(os == 2)
         system("clear");
-    }
 }
