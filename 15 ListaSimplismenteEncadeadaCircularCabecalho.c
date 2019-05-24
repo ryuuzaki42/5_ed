abb7965ad14a14c1cc2310b6113d3ff7d3d13d31
João D. Batista <joao42lbatista@gmail.com>
2019-05-24 11:29:21 -0300

    15 ListaSimplismenteEncadeadaCircularCabecalho.c - gets to scanf

--- 15 ListaSimplismenteEncadeadaCircularCabecalho.c
+++ 15 ListaSimplismenteEncadeadaCircularCabecalho.c
@@ -212,7 +212,7 @@ int main() {
 
             case 1: // Inserir no Inicio
                 printf("Digite o nome do aluno: ");
-                gets(a.nome);
+                scanf("%s", a.nome);
                 printf("Digite a idade do aluno: ");
                 scanf("%d", &a.idade);
 
@@ -225,7 +225,7 @@ int main() {
 
             case 2: // Inserir no final
                 printf("Digite o nome do aluno: ");
-                gets(a.nome);
+                scanf("%s", a.nome);
                 printf("Digite a idade do aluno: ");
                 scanf("%d", &a.idade);
 
