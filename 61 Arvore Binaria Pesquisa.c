830361daf92b9a82efe5a91cc3b3bee13dfe2f4d
João D. Batista <joao42lbatista@gmail.com>
2019-05-24 10:55:57 -0300

    61 Arvore Binaria Pesquisa.c - indent --style=java

--- 61 Arvore Binaria Pesquisa.c
+++ 61 Arvore Binaria Pesquisa.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct sNo{
+typedef struct sNo {
     int info;
     struct sNo *esq;
     struct sNo *dir;
-}No;
+} No;
 
-void inicializarArvore(No **ptRaiz){
+void inicializarArvore(No **ptRaiz) {
     *ptRaiz = NULL;
 }
 
-void insereComRecursao(No **ptRaiz, int info2){ //Inserção com recursão
-    if (*ptRaiz == NULL){
+void insereComRecursao(No **ptRaiz, int info2) { // Inserção com recursão
+    if(*ptRaiz == NULL) {
         *ptRaiz=(No *)malloc(sizeof(No));
-        if(*ptRaiz == NULL){
+
+        if(*ptRaiz == NULL) {
             printf("\nErro.Memoria nao alocada");
             return;
         }
@@ -22,21 +23,20 @@ void insereComRecursao(No **ptRaiz, int info2){ //Inserção com recursão
         (*ptRaiz)->esq=NULL;
         (*ptRaiz)->dir=NULL;
         (*ptRaiz)->info=info2;
-    }else{
-        if (info2 < ((*ptRaiz)->info)){
+    } else {
+        if(info2 < ((*ptRaiz)->info))
             insereComRecursao(&((*ptRaiz)->esq), info2);
-        } else {
+        else
             insereComRecursao(&((*ptRaiz)->dir), info2);
-        }
     }
 }
 
-void insereSemRecursao(No** ptRaiz, int info2){ //Inserção com recursão
+void insereSemRecursao(No** ptRaiz, int info2) { // Inserção com recursão
     No* aux = *ptRaiz;
     No* aux2 = NULL;
     No* novo = (No *)malloc(sizeof(No));
 
-    if(novo == NULL){
+    if(novo == NULL) {
         printf("\nErro: Memoria nao alocada");
         return;
     }
@@ -45,99 +45,99 @@ void insereSemRecursao(No** ptRaiz, int info2){ //Inserção com recursão
     novo->esq = NULL;
     novo->dir = NULL;
 
-    while(aux != NULL){
+    while(aux != NULL) {
         aux2 = aux;
 
-        if(info2 < aux->info){
+        if(info2 < aux->info)
             aux = aux->esq;
-        }else{
+        else
             aux = aux->dir;
-        }
     }
 
-    if(aux2 == NULL){
+    if(aux2 == NULL)
         *ptRaiz = novo;
-    }else {
-        if(info2 < aux2->info){
+    else {
+        if(info2 < aux2->info)
             (aux2)->esq = novo;
-        }else{
+        else
             (aux2)->dir = novo;
-        }
     }
 }
 
-void maiorValor(No* ptRaiz){
-    if((ptRaiz->dir != NULL) && (ptRaiz->dir->info > ptRaiz->info)){
+void maiorValor(No* ptRaiz) {
+    if((ptRaiz->dir != NULL) && (ptRaiz->dir->info > ptRaiz->info))
         maiorValor(ptRaiz->dir);
-    }else{
+    else
         printf("Maior Valor: %d\n", ptRaiz->info);
-    }
 }
 
-void menorValor(No* ptRaiz){
-    if((ptRaiz->esq != NULL) && (ptRaiz->esq->info < ptRaiz->info)){
+void menorValor(No* ptRaiz) {
+    if((ptRaiz->esq != NULL) && (ptRaiz->esq->info < ptRaiz->info))
         menorValor(ptRaiz->esq);
-    }else{
+    else
         printf("Menor Valor: %d\n", ptRaiz->info);
-    }
 }
 
-No *MaiorDireita(No **no){ //Remoção
-    if((*no)->dir != NULL){
-       return MaiorDireita(&(*no)->dir);
-    }else{
+No *MaiorDireita(No **no) { // Remoção
+    if((*no)->dir != NULL)
+        return MaiorDireita(&(*no)->dir);
+    else {
         No *aux = *no;
 
-        if((*no)->esq != NULL){ // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esq!
+        if((*no)->esq != NULL) // Se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esq!
             *no = (*no)->esq;
-        }else{
+        else {
             *no = NULL;
             return aux;
         }
     }
 }
 
-No *MenorEsquerda(No **no){
-    if((*no)->esq != NULL){
+No *MenorEsquerda(No **no) {
+    if((*no)->esq != NULL)
         return MenorEsquerda(&(*no)->esq);
-    }else{
+    else {
         No *aux = *no;
-        if((*no)->dir != NULL) { // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da dir!
+
+        if((*no)->dir != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da dir!
             *no = (*no)->dir;
-        }else{
+        else
             *no = NULL;
-        }
+
         return aux;
     }
 }
 
-void remover(No **ptRaiz, int info2){
-    if(*ptRaiz == NULL){ // esta verificacao serve para caso o info nao exista na arvore.
+void remover(No **ptRaiz, int info2) {
+    if(*ptRaiz == NULL) { // Esta verificacao serve para caso o info nao exista na arvore.
         printf("Numero nao existe na arvore!");
         return;
     }
 
-    if(info2 < (*ptRaiz)->info){
+    if(info2 < (*ptRaiz)->info)
         remover(&(*ptRaiz)->esq, info2);
-    }else{
-        if(info2 > (*ptRaiz)->info){
+    else {
+        if(info2 > (*ptRaiz)->info)
             remover(&(*ptRaiz)->dir, info2);
-        }else{ // se nao eh menor nem maior, logo, eh o info que estou procurando! :)
+        else { // se nao eh menor nem maior, logo, eh o info que estou procurando! :)
             No *pAux = *ptRaiz; // quem programar no Embarcadero vai ter que declarar o pAux no inicio do void! :[
-            if(((*ptRaiz)->esq == NULL) && ((*ptRaiz)->dir == NULL)){ // se nao houver filhos...
+
+            if(((*ptRaiz)->esq == NULL) && ((*ptRaiz)->dir == NULL)) { // se nao houver filhos...
                 free(pAux);
                 (*ptRaiz) = NULL;
-            }else{ // so tem o filho da dir
-                if((*ptRaiz)->esq == NULL){
+            } else { // so tem o filho da dir
+                if((*ptRaiz)->esq == NULL) {
                     (*ptRaiz) = (*ptRaiz)->dir;
                     pAux->dir = NULL;
-                    free(pAux); pAux = NULL;
-                }else{ //so tem filho da esq
-                    if ((*ptRaiz)->dir == NULL){
+                    free(pAux);
+                    pAux = NULL;
+                } else { // so tem filho da esq
+                    if((*ptRaiz)->dir == NULL) {
                         (*ptRaiz) = (*ptRaiz)->esq;
                         pAux->esq = NULL;
-                        free(pAux); pAux = NULL;
-                    }else{ //Escolhi fazer o maior filho direito da subarvore esq.
+                        free(pAux);
+                        pAux = NULL;
+                    } else { // Escolhi fazer o maior filho direito da subarvore esq.
                         pAux = MaiorDireita(&(*ptRaiz)->esq); //se vc quiser usar o Menor da esq, so o que mudaria seria isso:
                         pAux->esq = (*ptRaiz)->esq; // pAux = MenorEsquerda(&(*ptRaiz)->dir);
                         pAux->dir = (*ptRaiz)->dir;
@@ -152,135 +152,128 @@ void remover(No **ptRaiz, int info2){
     }
 }
 
-void printTrace(int level){
-    for (int i=0;i<level;i++){
+void printTrace(int level) {
+    for(int i=0; i<level; i++)
         printf("-");
-    }
 }
 
-void imprimirArvore(No* ptRaiz, int level){
-    if(ptRaiz == NULL){
+void imprimirArvore(No* ptRaiz, int level) {
+    if(ptRaiz == NULL) {
         printTrace(level);
         printf("*\n");
         return;
     }
 
-    imprimirArvore(ptRaiz->dir, level+1); 
-
+    imprimirArvore(ptRaiz->dir, level+1);
     printTrace(level);
     printf("%d\n", ptRaiz->info);
-
     imprimirArvore(ptRaiz->esq, level+1);
 }
 
-void visita(No *ptRaiz){
+void visita(No *ptRaiz) {
     printf("%d\n",ptRaiz->info);
 }
 
-void exibirEmOrdem(No *ptRaiz){ //Em ordem
-    if(ptRaiz != NULL){
+void exibirEmOrdem(No *ptRaiz) { // Em ordem
+    if(ptRaiz != NULL) {
         exibirEmOrdem(ptRaiz->esq);
         visita(ptRaiz);
         exibirEmOrdem(ptRaiz->dir);
     }
 }
 
-void exibirPreOrdem(No *ptRaiz){ //Pré-ordem
-    if(ptRaiz != NULL){
+void exibirPreOrdem(No *ptRaiz) { // Pré-ordem
+    if(ptRaiz != NULL) {
         visita(ptRaiz);
         exibirPreOrdem(ptRaiz->esq);
         exibirPreOrdem(ptRaiz->dir);
     }
 }
 
-void exibirPosOrdem(No *ptRaiz){ //Pós-ordem
-    if(ptRaiz != NULL){
+void exibirPosOrdem(No *ptRaiz) { // Pós-ordem
+    if(ptRaiz != NULL) {
         exibirPosOrdem(ptRaiz->esq);
         exibirPosOrdem(ptRaiz->dir);
         visita(ptRaiz);
     }
 }
 
-int contarNos(No *ptRaiz){ //Contar nós
-    if(ptRaiz == NULL){
+int contarNos(No *ptRaiz) { // Contar nós
+    if(ptRaiz == NULL)
         return 0;
-    }else{
+    else
         return 1 + contarNos(ptRaiz->esq) + contarNos(ptRaiz->dir);
-    }
 }
 
-int contarFolhas(No *ptRaiz){ //Contar folhas
-    if(ptRaiz == NULL){
+int contarFolhas(No *ptRaiz) { // Contar folhas
+    if(ptRaiz == NULL)
         return 0;
-    }
 
-    if(ptRaiz->esq == NULL && ptRaiz->dir == NULL){
+    if(ptRaiz->esq == NULL && ptRaiz->dir == NULL)
         return 1;
-    }
 
     return contarFolhas(ptRaiz->esq) + contarFolhas(ptRaiz->dir);
 }
 
-int maior(int a, int b){
-    if(a > b){
+int maior(int a, int b) {
+    if(a > b)
         return a;
-    }else{
+    else
         return b;
-    }
 }
 
-int alturaArvore(No *ptRaiz){ //Altura da árvore
-    if((ptRaiz == NULL) || (ptRaiz->esq == NULL && ptRaiz->dir == NULL)){
+int alturaArvore(No *ptRaiz) { // Altura da árvore
+    if((ptRaiz == NULL) || (ptRaiz->esq == NULL && ptRaiz->dir == NULL))
         return 0;
-    }else{
+    else
         return 1 + maior(alturaArvore(ptRaiz->esq), alturaArvore(ptRaiz->dir));
-    }
 }
 
-int procuraElementoSemRecursao(No *ptRaiz, int info2){ // Procura o elemento na árvore // O(log n)
+int procuraElementoSemRecursao(No *ptRaiz, int info2) { // Procura o elemento na árvore // O(log n)
     No* temp = ptRaiz;
     int parar=0;
 
-    if(ptRaiz==NULL){
+    if(ptRaiz==NULL)
         return 0;
-    }else if(temp->esq== NULL && temp->dir==NULL){
-        if(temp->info == info2){
+    else if(temp->esq== NULL && temp->dir==NULL) {
+        if(temp->info == info2)
             return 1;
-        }else{
+        else
             return 0;
-        }
-    }else if(temp->info==info2){
+    } else if(temp->info==info2)
         return 1;
-    }else{
-        while(parar==0){
-            if(temp->info < info2){
-                if(temp->esq == NULL){
+    else {
+        while(parar==0) {
+            if(temp->info < info2) {
+                if(temp->esq == NULL) {
                     parar++;
-                    if (temp->info==info2){
+
+                    if(temp->info==info2) {
                         break;
                         return 1;
-                    }else{
+                    } else
                         return 0;
-                    }
-                }else{
+                } else {
                     temp=temp->esq;
-                        if (temp->info== info2){
-                            return 1;
-                            break;
+
+                    if(temp->info== info2) {
+                        return 1;
+                        break;
                     }
                 }
-            }else if(temp->info > info2){
-                if(temp->dir== NULL){
+            } else if(temp->info > info2) {
+                if(temp->dir== NULL) {
                     parar++;
-                    if (temp->info==info2){
+
+                    if(temp->info==info2) {
                         return 1;
                         break;
-                    }else{
+                    } else
                         return 0;
-                    }
-                }else{
+                } else {
                     temp=temp->dir;
-                    if (temp->info== info2){
+
+                    if(temp->info== info2) {
                         return 1;
                         break;
                     }
@@ -290,28 +283,26 @@ int procuraElementoSemRecursao(No *ptRaiz, int info2){ // Procura o elemento na 
     }
 }
 
-No* procuraElementoComRecursao(No *ptRaiz, int info2){ // Procura o elemento na árvore // O(log n)
+No* procuraElementoComRecursao(No *ptRaiz, int info2) { // Procura o elemento na árvore // O(log n)
     No* aux;
     aux = ptRaiz;
 
-    if(aux == NULL){
+    if(aux == NULL)
         return NULL;
-    }else if(info2 < aux->info){
+    else if(info2 < aux->info)
         return procuraElementoComRecursao(aux->esq, info2);
-    }else if(info2 > aux->info){
+    else if(info2 > aux->info)
         return procuraElementoComRecursao(aux->dir, info2);
-    }else{
+    else
         return aux;
-    }
 }
 
-int main(){
+int main() {
     int info2, opcao;
     No *ptRaiz;
-
     inicializarArvore(&ptRaiz);
 
-    while(1){
+    while(1) {
         printf("\nArvore Binaria de Pesquisada");
         printf("\n 1 insere sem recursao");
         printf("\n 2 insere com recursao");
@@ -330,74 +321,89 @@ int main(){
         printf("\n 0 Para sair do programa");
         printf("\nDigite a opcao desejada: ");
         scanf("%d", &opcao);
-
         printf("\n");
-        switch(opcao){
+
+        switch(opcao) {
             case 1:
                 printf("Numero a ser inserido: ");
                 scanf("%d", &info2);
                 insereComRecursao(&ptRaiz, info2);
                 break;
+
             case 2:
                 printf("Numero a ser inserido: ");
                 scanf("%d", &info2);
                 insereSemRecursao(&ptRaiz, info2);
                 break;
+
             case 3:
                 printf("Numero a ser remvido: ");
                 scanf("%d", &info2);
                 remover(&ptRaiz, info2);
                 break;
+
             case 4:
                 exibirPreOrdem(ptRaiz);
                 break;
+
             case 5:
                 exibirEmOrdem(ptRaiz);
                 break;
+
             case 6:
                 exibirPosOrdem(ptRaiz);
                 break;
+
             case 7:
                 printf("Quantidade de folhas: %d", contarFolhas(ptRaiz));
                 break;
+
             case 8:
                 printf("Quantidade de nos: %d", contarNos(ptRaiz));
                 break;
+
             case 9:
                 printf("Altura da arvore: %d", alturaArvore(ptRaiz));
                 break;
+
             case 10:
                 printf("Numero a ser pesquisado: ");
                 scanf("%d", &info2);
 
-                if(procuraElementoSemRecursao(ptRaiz, info2) == 1){
+                if(procuraElementoSemRecursao(ptRaiz, info2) == 1)
                     printf("\n\nElemento %d encontrado", info2);
-                }else{
+                else
                     printf("\n\nElemento nao %d encontrado", info2);
-                }
+
                 break;
+
             case 11:
                 printf("Numero a ser pesquisado: ");
                 scanf("%d", &info2);
 
-                if(procuraElementoComRecursao(ptRaiz, info2) != NULL){
+                if(procuraElementoComRecursao(ptRaiz, info2) != NULL)
                     printf("\n\nElemento %d encontrado", info2);
-                }else{
+                else
                     printf("\n\nElemento nao %d encontrado", info2);
-                }
+
                 break;
+
             case 12:
                 imprimirArvore(ptRaiz, 0);
                 break;
+
             case 13:
                 maiorValor(ptRaiz);
                 break;
+
             case 14:
                 menorValor(ptRaiz);
                 break;
+
             case 0:
                 exit(0);
                 break;
+
             default:
                 printf("A opcao: %d e invalida", opcao);
                 break;
