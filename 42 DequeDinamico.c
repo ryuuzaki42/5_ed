2527f1332c2a71360d372dd7122b0f3ff5b8f38c
João D. Batista <joao42lbatista@gmail.com>
2019-05-24 10:54:56 -0300

    42 DequeDinamico.c - indent --style=java

--- 42 DequeDinamico.c
+++ 42 DequeDinamico.c
@@ -1,213 +1,237 @@
 #include<stdio.h>
 #include<string.h>
 
-#include "0 os.h" //To use clean_output() and clean_stdin()
+#include "0 os.h" // To use clean_output() and clean_stdin()
 
-typedef struct sAluno{
+typedef struct sAluno {
     char nome[100];
     int idade;
-}Aluno;
+} Aluno;
 
-typedef struct sCelula{
+typedef struct sCelula {
     Aluno info;
     struct sCelula *prox, *ante;
-}Celula;
+} Celula;
 
-typedef struct sDeque{
+typedef struct sDeque {
     Celula *inicio, *fim;
-}Deque;
+} Deque;
 
-void inicializar (Deque *deque){
+void inicializar(Deque *deque) {
     deque->inicio = NULL;
     deque->fim = NULL;
 }
 
-int dequeVazio(Deque *deque){
-    if(deque->inicio == NULL && deque->fim == NULL){
+int dequeVazio(Deque *deque) {
+    if(deque->inicio == NULL && deque->fim == NULL)
         return 1;
-    }
+
     return 0;
 }
 
-Celula* criarcelula(){
+Celula* criarcelula() {
     return (Celula*)malloc(sizeof(Celula));
 }
 
-Aluno criarAluno(char nome[], int idade){
+Aluno criarAluno(char nome[], int idade) {
     Aluno e;
     strcpy(e.nome, nome);
     e.idade = idade;
     return e;
 }
 
-int inserirNoInicio(Deque* deque, Aluno novo){
+int inserirNoInicio(Deque* deque, Aluno novo) {
     Celula *novacelula = criarcelula();
-    if(novacelula == NULL){
+
+    if(novacelula == NULL)
         return 0;
-    }
+
     novacelula->info = novo;
     novacelula->prox = NULL;
     novacelula->ante = NULL;
-    if(dequeVazio(deque)){
+
+    if(dequeVazio(deque)) {
         deque->inicio = novacelula;
         deque->fim = novacelula;
         return 1;
     }
+
     novacelula->prox = deque->inicio;
     deque->inicio->ante = novacelula;
     deque->inicio = novacelula;
     return 1;
 }
 
-int inserirNoFim(Deque *deque, Aluno novo){
+int inserirNoFim(Deque *deque, Aluno novo) {
     Celula *novacelula = criarcelula();
-    if(novacelula == NULL){ return 0; }
+
+    if(novacelula == NULL)
+        return 0;
+
     novacelula->info = novo;
     novacelula->prox = NULL;
-    novacelula->ante = NULL; 
-    if(dequeVazio(deque)){
+    novacelula->ante = NULL;
+
+    if(dequeVazio(deque)) {
         deque->inicio = novacelula;
         deque->fim = novacelula;
         return 1;
     }
+
     novacelula->ante = deque->fim;
     deque->fim->prox = novacelula;
     deque->fim = novacelula;
     return 1;
 }
 
-Aluno removerNoInicio(Deque *deque){
+Aluno removerNoInicio(Deque *deque) {
     Aluno vazio = criarAluno("",-1);
-    if(dequeVazio(deque)){
+
+    if(dequeVazio(deque))
         return vazio;
-    }
+
     Celula *aux = deque->inicio;
     vazio = aux->info;
-    if(deque->inicio == deque->fim){
+
+    if(deque->inicio == deque->fim) {
         inicializar(deque);
         free(aux);
         return vazio;
     }
+
     deque->inicio = deque->inicio->prox;
     deque->inicio->ante  = NULL;
     free(aux);
     return vazio;
 }
 
-Aluno removerNoFim(Deque *deque){
+Aluno removerNoFim(Deque *deque) {
     Aluno vazio = criarAluno("",-1);
-    if(dequeVazio(deque)){
+
+    if(dequeVazio(deque))
         return vazio;
-    }
+
     Celula* aux = deque->fim;
     vazio = aux->info;
-    if(deque->inicio == deque->fim){
+
+    if(deque->inicio == deque->fim) {
         inicializar(deque);
         free(aux);
         return vazio;
     }
+
     deque->fim = deque->fim->ante;
     deque->fim->prox = NULL;
     free(aux);
     return vazio;
 }
 
-void imprimirTodos(Deque *deque){
+void imprimirTodos(Deque *deque) {
     Aluno aux = criarAluno("",-1);
     Deque deque2;
     inicializar(&deque2);
-    if(dequeVazio(deque)){
+
+    if(dequeVazio(deque)) {
         printf("\nDeque Vazio :(\n");
         return;
     }
-    while(!dequeVazio(deque)){
+
+    while(!dequeVazio(deque)) {
         aux = removerNoInicio(deque);
         printf("Nome: %s - %d\n", aux.nome, aux.idade);
         inserirNoFim(&deque2, aux);
     }
-    while(!dequeVazio(&deque2)){
+
+    while(!dequeVazio(&deque2))
         inserirNoFim(deque, removerNoInicio(&deque2));
-    }
 }
 
-void Menu(){
+void Menu() {
     printf("\n::INSERIR\n");
     printf("  [1] - Inserir No Inicio\n");
     printf("  [2] - Inserir No Fim\n");
-
     printf("\n::REMOVER\n");
     printf("  [3] - Remover No Inicio\n");
     printf("  [4] - Remover No Fim\n");
-
     printf("\n::MOSTRAR\n");
     printf("  [5] - Imprimir\n\n");
-
     printf("::SAIR\n");
     printf("  [0] - Sair\n\n");
 }
 
-int main(){
+int main() {
     Aluno e;
     Deque deque;
     inicializar(&deque);
     int option;
-    do{
+
+    do {
         Menu();
         printf("Digite uma opcao: ");
         scanf("%d", &option);
-
         clean_output();
         clean_stdin();
 
-        switch(option){
+        switch(option) {
             case 0:
                 return 0;
+
             case 1:
                 printf("Digite o nome: ");
                 scanf("%s", e.nome);
                 printf("Digite o idade: ");
                 scanf("%d", &e.idade);
-                if(inserirNoInicio(&deque, e)){
+
+                if(inserirNoInicio(&deque, e))
                     printf("\nAluno inserido :)\n");
-                }else{
+                else
                     printf("\nOcorreu um erro :(");
-                }
+
                 break;
+
             case 2:
                 printf("Digite o nome: ");
                 scanf("%s", e.nome);
                 printf("Digite o idade: ");
                 scanf("%d", &e.idade);
-                if(inserirNoFim(&deque, e)){
+
+                if(inserirNoFim(&deque, e))
                     printf("\nAluno inserido :)\n");
-                }else{
+                else
                     printf("\nOcorreu um erro :(\n");
-                }
+
                 break;
+
             case 3:
                 e = removerNoInicio(&deque);
-                if(e.idade != -1){
+
+                if(e.idade != -1)
                     printf("\nAluno retirado: %s\n",e.nome);
-                }else{
+                else
                     printf("\nDeque Vazio :(\n");
-                }
+
                 imprimirTodos(&deque);
                 break;
+
             case 4:
                 e = removerNoFim(&deque);
-                if(e.idade != -1){
+
+                if(e.idade != -1)
                     printf("\nAluno retirado: %s\n",e.nome);
-                }else{
+                else
                     printf("\nDeque Vazio :(\n");
-                }
+
                 imprimirTodos(&deque);
                 break;
+
             case 5:
                 imprimirTodos(&deque);
                 break;
+
             default:
                 printf("\nOpcao invalida!\n");
         }
+
         printf("\n");
-    }while(1);
+    } while(1);
 }
