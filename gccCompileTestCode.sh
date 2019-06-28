2613c2372cd70a57fffc6806193c2621bcd76e1b
João D. Batista <joao42lbatista@gmail.com>
2019-06-28 18:47:00 -0300

    gccCompileTestCode.sh add

---
+++ gccCompileTestCode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+IFSOLD=$IFS
+
+IFS=$(echo -en "\\n\\b") # Change the Internal Field Separator (IFS) to "\\n\\b"
+
+stdVersion="c99" # c99 c11 c17 c18
+
+for file in $(ls *.c); do
+    echo "gcc -Wall -Wextra -Wpedantic -O3 -ansi -std=$stdVersion $file -o test.out"
+    gcc -Wall -Wextra -Wpedantic -O3 -ansi -std=$stdVersion "$file" -o test.out
+done
+
+IFS=$IFSOLD
