3a155e0b172933ba1a5796f696a3d74b9382ce00
João D. Batista <joao42lbatista@gmail.com>
2020-11-17 22:36:50 -0300

    gccCompileTestCode.sh - change to use -std=c99

--- gccCompileTestCode.sh
+++ gccCompileTestCode.sh
@@ -3,15 +3,15 @@ IFSOLD=$IFS
 
 IFS=$(echo -en "\\n\\b") # Change the Internal Field Separator (IFS) to "\\n\\b"
 
-# stdVersion="c99" # c99 c11 c17 c18
+stdVersion="c99" # c89 c99 c11 c17 c18
 
 for file in $(ls *.c); do
-#     echo "gcc -Wall -Wextra -Wpedantic -O3 -std=$stdVersion $file -o test.out"
-#     gcc -Wall -Wextra -Wpedantic -O3 -std=$stdVersion "$file" -o test.out
+    echo "gcc -Wall -Wextra -Wpedantic -O3 -std=$stdVersion \"$file\" -o test.out"
+    gcc -Wall -Wextra -Wpedantic -O3 -std=$stdVersion "$file" -o test.out
 
     # or
-    echo "gcc -Wall -Wextra -Wpedantic -O3 -ansi $file -o test.out"
-    gcc -Wall -Wextra -Wpedantic -O3 -ansi "$file" -o test.out
+#     echo "gcc -Wall -Wextra -Wpedantic -O3 -ansi \"$file\" -o test.out"
+#     gcc -Wall -Wextra -Wpedantic -O3 -ansi "$file" -o test.out
 done
 
 IFS=$IFSOLD
